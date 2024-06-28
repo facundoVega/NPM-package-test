8cd9af0d58426478f2ddcb71cc4ce94b863be9a6
Horacio Vega <2187815@corp.trizetto.com>
2024-06-28 17:14:32 -0300

    export issue fixed.

--- projects/shared-library/src/lib/store/utils.ts
+++ projects/shared-library/src/lib/store/utils.ts
@@ -1,8 +1,9 @@
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, StoreRootModule } from '@ngrx/store';
 import { MSAL_FEATURE_KEY, msalReducer } from './reducers';
+import { ModuleWithProviders } from '@angular/core';
 
 
-export function getStoreModule(): any {
+export function getStoreModule(): ModuleWithProviders<StoreRootModule> {
     return StoreModule.forRoot({
         [MSAL_FEATURE_KEY]: msalReducer});
 }
