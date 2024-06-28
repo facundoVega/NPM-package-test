87c62f169db11a912f5329ac1ffd9c19105be975
facundoVega <horacio_facundo.vega@hotmail.com>
2024-06-28 14:15:41 -0300

    sidebar and footer component

--- projects/shared-library/src/public-api.ts
+++ projects/shared-library/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/shared-library.service';
 export * from './lib/shared-library.component';
 export * from './lib/header/header.component'
+export * from './lib/footer/footer.component'
+export * from './lib/sidebar/sidebar.component'
