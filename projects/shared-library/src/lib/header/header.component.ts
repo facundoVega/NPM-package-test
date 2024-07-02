0e3a176607fec42119c4b06a651929eb8d87a41c
Horacio Vega <2187815@corp.trizetto.com>
2024-07-02 18:12:00 -0300

    Using store on header component.

--- projects/shared-library/src/lib/header/header.component.ts
+++ projects/shared-library/src/lib/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { activeUSer } from "../store/selectors";
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'header-component',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
   })
-  export class HeaderComponent {
+  export class HeaderComponent implements OnInit {
+    @Input() store!: Store;
+    $user!: Observable<string | undefined>
+    ngOnInit(): void {
+      if(this.store)  {
+        this.$user =  this.store.select(activeUSer)
+      }
+      
+    }
   }
